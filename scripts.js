189a3e42d8440f2e3ea2bfbd9a158cba7a26f052
novraburv <78609440+novraburv@users.noreply.github.com>
2022-01-23 17:03:48 +0700

    add gameBoard module

--- scripts.js
+++ scripts.js
@@ -1,1 +1,18 @@
 "use strict"
+
+const gameBoard = (() => {
+	const board = new Array(9);
+
+	function getBoard() {
+		return board;
+	}
+	function getCell(index) {
+		return board[index];
+	}
+	function setCell(index, value) {
+		board[index] = value;
+	}
+	return { getBoard, getCell, setCell }
+})();
+const displayController = (() => {})();
+const gameController = (() => {})();
