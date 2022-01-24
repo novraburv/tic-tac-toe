fa0cb7f6a2330cc043f57fdfef4952ad658f5b3e
novraburv <78609440+novraburv@users.noreply.github.com>
2022-01-24 16:50:37 +0700

    add Player factory and chechEnd()

--- scripts.js
+++ scripts.js
@@ -27,11 +27,16 @@ const gameBoard = (() => {
 const displayController = (() => {
 	const board = document.querySelector('.board');
 	const cells = board.querySelectorAll('.cell');
-	const resetButton = document.querySelector('.btn-reset');
 	const nextButton = document.querySelector('.btn-next');
+	const reloadButton = document.querySelector('.btn-reload');
+	const resetButton = document.querySelector('.btn-reset');
 	const activePlayer = document.querySelector('.activePlayer');
 	const winningBanner = document.querySelector('.winning-announcement');
 	const winningMsg = document.querySelector('.winning-message');
+	const XScore = document.querySelector('.XScore');
+	const OScore = document.querySelector('.OScore');
+	const threeWinsEls = document.querySelectorAll('.three-wins-hidden');
+	const threeWinsMsg = document.querySelector('.three-wins-message');
 
 	function updateBoard() {
 		cells.forEach(cell => {
@@ -42,8 +47,9 @@ const displayController = (() => {
 		cells.forEach(cell => {
 			cell.addEventListener('click', clickCell);
 		})
-		resetButton.addEventListener('click', reset);
 		nextButton.addEventListener('click', next);
+		reloadButton.addEventListener('click', reload);
+		resetButton.addEventListener('click', reset);
 	}
 	function clickCell(e) {
 		if (gameBoard.getCell(e.target.dataset.index)) return;
@@ -51,6 +57,10 @@ const displayController = (() => {
 		updateBoard();
 		updateActivePlayer();
 		checkWinner();
+		checkEnd();
+	}
+	function reload() {
+		location.reload();
 	}
 	function reset() {
 		gameBoard.reset();
@@ -71,12 +81,27 @@ const displayController = (() => {
 		if (!result) return;
 		winningMsg.innerHTML = '';
 		if (result === 'X') {
+			X.addScore();
+			XScore.textContent = X.getScore();
 			winningMsg.innerHTML = `Congratulation! <span class="activePlayer">${result}</span> won this round.`;
 		}
 		if (result === 'O') {
+			O.addScore();
+			OScore.textContent = O.getScore();
 			winningMsg.innerHTML = `How unfortunate! <span class="activePlayer">${result}</span> won this round.`;
 		}
 		winningBanner.style.display = 'flex';
+	}
+	function checkEnd() {
+		const Xnum = X.getScore();
+		const Onum = O.getScore();
+		if (Xnum === 3 || Onum === 3) {
+			threeWinsMsg.textContent = 'The game has been dragged for too long. We will end this. Click Reload.'
+			threeWinsEls.forEach(el => {
+				el.style.display = 'block';
+			})
+			nextButton.style.display = 'none';
+		}
 	}
 	return { updateBoard, startUp }
 })();
@@ -120,5 +145,24 @@ const gameController = (() => {
 	return { getPlayer, setPlayer, changePlayer, evaluate }
 })();
 
+// Factory function to save players' data
+function Player(sign) {
+	const icon = sign;
+	let score = 0;
+
+	function getScore() {
+		return score;
+	}
+	function addScore() {
+		score++;
+	}
+	function getSign() {
+		return icon;
+	}
+	return { getScore, addScore, getSign }
+}
+
+const X = Player('X');
+const O = Player('O');
 
 displayController.startUp();
